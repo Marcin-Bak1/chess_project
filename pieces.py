e3ce3d44358462e5fddd407241eed4e4dbc586cf
Marcin-Bak1 <kontakt.marcin.bak@gmail.com>
2019-07-11 21:50:55 +0100

    humble beginnings

---
+++ pieces.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class pawn:
+    def __init__(self, x, y, side):
+        self.posx = x
+        self.posy = y
+        self.color = side
+    
+    
